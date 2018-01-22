cbec09573180471ba0dc0d346971d72bf4f30bc2
Lei Wamg <2308170w@student.gla.ac.uk>
2018-01-22 22:15:41 +0800

    Chapter4_After setting up database

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+#Represent Category Model
+class Category(models.Model):
+    name=models.CharField(max_length=128, unique=True)
+
+    def __str__(self):  #For Python 2, use __unicode__ too
+            return self.name
+
+
+#Represent Page Model
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self): #For Python 2, use __unicode__ too
+        return self.title
